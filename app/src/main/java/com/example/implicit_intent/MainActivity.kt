80e0ca9d19c2cf1844b88222d7154c08450e0bbd
alvinda0 <alvindashahrul0@gmail.com>
2023-03-29 23:32:26 +0800

    first commit

---
+++ app/src/main/java/com/example/implicit_intent/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.implicit_intent
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.Btn)
+
+
+        btn.setOnClickListener{
+            Intent(Intent.ACTION_GET_CONTENT).also {
+                it.type = "image/*"
+                startActivityForResult(it,0)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == 0) {
+            val uri = data?.data
+            val img = findViewById<ImageView>(R.id.img)
+            img.setImageURI(uri)
+        }
+    }
+}
